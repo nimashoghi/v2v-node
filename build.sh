08d51690365db49cf484f3700f9a0649d26bef7b
Nima Shoghi <nimashoghi@gmail.com>
2020-03-03 21:52:32 +0000

    Updated file structure

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yarn install && yarn build:scratch
-
 docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t nimashoghi/v2v-node . --push
--- src/patch-console-log.ts
+++
@@ -1,7 +0,0 @@
-import {assertDefined} from "./util"
-
-export const patchConsoleLog = () => {
-    const logDefault = globalThis.console.log
-    globalThis.console.log = (...args: any[]) =>
-        logDefault(`[${assertDefined(process.env.MY_TOPIC)}]`, ...args)
-}
